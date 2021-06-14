2c2b446906f0821b4133b56a9049549196555f44
piltr <piltriisola7@gmail.com>
2021-06-14 15:46:37 -0300

    error al buscar el id en la bd

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -32,7 +32,18 @@ var mainView = app.views.create('.view-main');
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
-   
+    var db = firebase.firestore()
+    colPersonas =  db.collection("personas")
+    var miid = "asd@as.as"
+   var data = {nombre : "pepe", mail : "pepe@asd.as",rol : "administrador"};
+   console.log(data)
+   db.collection("personas").doc(miid).set(data)
+    .then( function (docRef){
+      console.log("el id es " + docRef.id)
+    })
+    .catch( function(error){
+      console.log("error" + error)
+    })
 });
 
 // Option 1. Using one 'page:init' handler for all pages
@@ -42,6 +53,25 @@ $$(document).on('page:init', function (e) {
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
+$$(document).on('page:init', '.page[data-name="index"]', function (e) {
+    // Do something here when page with data-name="about" attribute loaded and initialized
+    alert('Hello');
+    // crea usuario en firebase
+    //var email = "asda@jhasjhgas.com";
+    //var clave = "34562ijss";
+    //firebase.auth().createUserWithEmailAndPassword(email, clave)
+    //    .catch( function(error) {
+    //        console.error(error.code);
+    //        if (error.code == "auth/email-already-in-use") {
+    //            console.error("el mail ya existe...");
+    //        }
+    //        //console.error(error.message);
+    //    } )
+    //    .then( function() {
+    //        console.log('que paso??');
+    //        mainView.router.navigate('/gracias/');
+    //    });
+})
 $$(document).on('page:init', '.page[data-name="about"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
     console.log(e);
@@ -54,7 +84,22 @@ $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
     $$("#registro").on("click", fun)
 
     function fun() {
-      alert("Ey!")
-      console.log("Ey!")
+      console.log("Registrando")
+      // crea usuario en firebase
+    var email = $$("#email").value();
+    var clave = $$("#password").value();
+    console.log(email, clave)
+    firebase.auth().createUserWithEmailAndPassword(email, clave)
+        .catch( function(error) {
+            console.error(error.code);
+            if (error.code == "auth/email-already-in-use") {
+                console.error("el mail ya existe...");
+           }
+            //console.error(error.message);
+        } )
+        .then( function() {
+            console.log('que paso??');
+            mainView.router.navigate('/gracias/');
+        });
     }
 })
